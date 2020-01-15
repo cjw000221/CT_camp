0e40dd6bbf22a780bafe127e0e46531f8d6e50b7
jwkim0417 <jwkim0417@postech.ac.kr>
2020-01-15 20:07:33 +0900

    fix typo

--- fsm_skeleton.cpp
+++ fsm_skeleton.cpp
@@ -1,7 +1,7 @@
 /*
-**Construct DFA with C programming**
+**Construct FSM with C programming**
 You know the current state, current input val
-If your dfa accept the state you should set your value result 1, if not 0
+If your fsm accept the state you should set your value result 1, if not 0
 Watch out the type of variables
 */
 
